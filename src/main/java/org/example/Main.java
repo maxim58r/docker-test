c70c1fef17f99e79bef7e7772528548cfc63a5a1
mserov <mserov@neoflex.ru>
2022-10-02 19:37:17 +0300

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Hello world!");
+        }
+    }
+}
